e360af5373ec0206fb249b1dd278d4ee3de90a27
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-04-18 00:54:52 +0100

    Write initial interface for linkedlist

---
+++ src/cpp/linkedlist/linkedlist.h
@@ -0,0 +1,18 @@
+﻿namespace mb_ds {
+    namespace linkedlist {
+        class Node {
+        public:
+          Node(int data): data{data}, next{nullptr} {}
+          Node(int data, Node* next): data{data}, next{next} {}
+
+          void append_to_tail(int data);
+
+          int data;
+          Node* next;
+        };
+
+        // deletes the first node with data data if it exists
+        Node* delete_node(int data, Node* head);
+    }
+}
+
