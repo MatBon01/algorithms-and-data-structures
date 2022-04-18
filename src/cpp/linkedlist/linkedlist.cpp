d0ee9ce109d0928700d12b3ab33b11b4df78606f
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-04-18 01:21:38 +0100

    Implement linkedlist append_to_tail

---
+++ src/cpp/linkedlist/linkedlist.cpp
@@ -0,0 +1,14 @@
+﻿#include "linkedlist.h"
+
+using namespace mb_ds;
+
+void linkedlist::Node::append_to_tail(int data) {
+    // TODO:: why namespace for node not found automatically
+    // TODO:: are you allowed to use new or am I doing it wrong?
+    // TODO:: must you delete the next element in the destructor if it exists?
+    if (next == nullptr) {
+        next = new mb_ds::linkedlist::Node(data);
+    } else {
+        next->append_to_tail(data);
+    }
+}
