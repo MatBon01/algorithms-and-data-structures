6728ecdf92527663cc4a7623a318a31cce9ab1e9
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-04-18 03:00:11 +0100

    Add client to linkedlist

---
+++ src/cpp/linkedlist/client.cpp
@@ -0,0 +1,22 @@
+﻿#include "linkedlist.h"
+#include <iostream>
+
+using namespace mb_ds;
+
+int main() {
+    linkedlist::Node* node = new linkedlist::Node {5};
+
+    node->append_to_tail(3);
+    node->append_to_tail(7);
+    node->append_to_tail(9);
+
+    linkedlist::delete_node(7, node);
+
+    linkedlist::Node* curr = node;
+    while (curr != nullptr) {
+        std::cout << curr->data << std::endl;
+        curr = curr->next;
+    }
+
+    delete node;
+}
